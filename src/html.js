2edb5c76be934365a60a7af1100c352ee38e02a8
choinashil <choinashil@gmail.com>
2020-10-06 20:00:05 +0900

    Change naver site verification

--- src/html.js
+++ src/html.js
@@ -16,6 +16,7 @@ export default function HTML(props) {
         {/* <link href="" rel="shortcut icon" type="image/x-icon" /> */}
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐙</text></svg>" />
         <link href="//cdn.jsdelivr.net/gh/wan2land/d2coding/d2coding-full.css" rel="stylesheet" />
+        <meta name="naver-site-verification" content="c92aeaad93883facee6965844dd0252f10f78930" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
